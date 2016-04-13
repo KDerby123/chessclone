45355fa0256314b6641250e4b67cd3b15f0c6526
KDerby123 <kika@taom.com>
2016-04-13 14:39:46 -0600

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -6,6 +6,8 @@ public class Pawn extends Piece {
 	}
 	
 	public abstract boolean testMove(Board b, int x, int y) { //I'll do this, as it has a weird move scheme
+	        Location l = b.getLocAt(x,y) ;
+	        Piece p = l.getPiece();
 		if (!super.notSameColor())
 		   return false;
 		   
