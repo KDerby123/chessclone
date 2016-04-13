55b0878897377ee6074d1b8906d26371f88e3874
KDerby123 <kika@taom.com>
2016-04-13 14:43:08 -0600

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -1,15 +1,15 @@
-
+//Moves diagonally, vertically, and horizontally
 public class Queen extends Piece {
 
     public Queen(String c,int x, int y) {
 	super(c,x,y);
     }
 	
-    public boolean testMove(Board b, int x, int y) { //Moves diagonally, vertically, and horizontally
+    public boolean testMove(Board b, int x, int y) { //takes in a Board, a x value, and a y checks if the move is valid
         Location l = b.getLocAt(x,y);
+        Piece p = l.getPiece(); //
         if (!super.notSameColor(l))
            return false;
-        Piece p = l.getPiece();
            
     }
 }
