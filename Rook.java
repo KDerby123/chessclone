4fe194617623a6c8a2be0d4ba8a4b8fbe812b1ee
KDerby123 <kika@taom.com>
2016-04-13 14:25:48 -0600

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,7 +1,15 @@
 
 public class Rook extends Piece {
 
-	public Rook(String c) {
-		super(c);
-	}
+    public Rook(String c,int x,int y) {
+	super(c,x,y);
+    }
+	
+    public boolean testMove(Board b, int x, int y) { //Moves horizontally and vertically
+        Location l = b.getLocAt(x,y);
+        if (!super.notSameColor(l))
+           return false;
+        Piece p = l.getPiece();
+           
+    }
 }
