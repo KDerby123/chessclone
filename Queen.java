1c27784baf1185eb000eb4aecb2d84cbc4f01b1e
KDerby123 <kika@taom.com>
2016-04-13 14:26:38 -0600

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -1,7 +1,15 @@
 
 public class Queen extends Piece {
 
-	public Queen(String c) {
-		super(c);
-	}
+    public Queen(String c,int x, int y) {
+	super(c,x,y);
+    }
+	
+    public boolean testMove(Board b, int x, int y) { //Moves in a L fashion, 1 square vertically, 2 squares horizontally
+        Location l = b.getLocAt(x,y);
+        if (!super.notSameColor(l))
+           return false;
+        Piece p = l.getPiece();
+           
+    }
 }
