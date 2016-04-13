2fa6da8ee0bd13e22a41d359eef16e45805b422f
KDerby123 <kika@taom.com>
2016-04-13 14:17:14 -0600

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -4,4 +4,10 @@ public class Pawn extends Piece {
 	public Pawn(String c) {
 		super(c);
 	}
+	
+	public abstract boolean testMove(Board b, int x, int y) { //I'll do this, as it has a weird move scheme
+		if (!super.notSameColor())
+		   return false;
+		   
+	}
 }
