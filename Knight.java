1f6b390f1514386db68175d6f304eb3bb72115cd
KDerby123 <kika@taom.com>
2016-04-13 14:22:57 -0600

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,4 +1,15 @@
 
 public class Knight {
 
+    public Knight(String c, int x, int y) {
+        super(c,x,y);
+    }
+    
+    public boolean testMove(Board b, int x, int y) { //Moves in a L fashion, 1 square vertically, 2 squares horizontally
+        Location l = b.getLocAt(x,y);
+        if (!super.notSameColor(l))
+           return false;
+        Piece p = l.getPiece();
+           
+    }
 }
