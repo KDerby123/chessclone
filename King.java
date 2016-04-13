930befbbad8a38a568f27da0572e868ddb3d9d29
KDerby123 <kika@taom.com>
2016-04-13 14:28:09 -0600

    Update King.java

--- King.java
+++ King.java
@@ -1,15 +1,19 @@
 
 public class King extends Piece {
 	
-	public King(String c) {
+    public King(String c) {
 		super(c);
-	}
+    }
 	
-	public ArrayList<String> getMoves() {
-		
-	}
+    public boolean testMove(Board b, int x, int y) { //I will do this one - Kika
+        Location l = b.getLocAt(x,y);
+        if (!super.notSameColor(l))
+           return false;
+        Piece p = l.getPiece();
+           
+    }
 	
-	public boolean isInCheck() {
+	public boolean isInCheck() { // This method will check if the king is in check
 		//code 
 		return false;
 	}
