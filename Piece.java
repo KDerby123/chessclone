c3d9f4146b33570577dbb9dae9f841868865a386
KDerby123 <kika@taom.com>
2016-04-13 14:10:31 -0600

    Update Piece.java
    #Piece Class v0.0.2  
    #The TestMove Commit  
    #Pull Request by KDerby123  
      
    --------------------------------  
      
    This pull request's main purpose is to introduce the abstract testMove() method into the Piece class. The testMove() method will be implemented in the Pawn, Bishop, Knight, Rook, Queen, and King classes, given that each piece moves differently. As a first test case, it will utilize the notSameColor() method (also implemented in the Piece class in this pull request) to filter whether the piece is of the same color.  
      
    ----------------------  
      
    End Pull Request.

--- Piece.java
+++ Piece.java
@@ -1,14 +1,41 @@
 import java.util.ArrayList;
-public abstract class Piece extends Location{
-	private int val;
+public abstract class Piece{
+        private String color;
+        private String notatLoc;
+	private int xLoc;
+        private int yLoc;
 	
 	
-	public Piece(int x, int y) {
-		super(x,y);
+	public Piece(String c, int x, int y) {
+		xLoc = x;
+                yLoc = y;
+                color = c;
+                notatLoc = Location.procCoords(x,y);
 	}
 	
+        public abstract boolean testMove(); //tests whether the move imported is a valid move
+
+        public boolean notSameColor(Location l) { //Tests whether the piece at the Location is of the same color, returns true if not
+                return ((l.getPiece() == null) || (!l.getPiece().getColor().equals(getColor())));
+        }
+
+        public String getColor() {
+                return color;
+        }
+        
+        public String getNotatLoc() {
+                return notatLoc;
+        }
+        
+        public int getX() {
+                return xLoc;
+        }
+       
+        public int getY() {
+                return yLoc;
+        }
 	
-	public abstract ArrayList<String> getMoves();
+	
 	
 
 }
