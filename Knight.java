08ad240e4047c3333c6f596ae8fe2036f8c7097f
KDerby123 <kika@taom.com>
2016-04-13 14:40:05 -0600

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -7,9 +7,10 @@ public class Knight {
     
     public boolean testMove(Board b, int x, int y) { //Moves in a L fashion, 1 square vertically, 2 squares horizontally
         Location l = b.getLocAt(x,y);
-        if (!super.notSameColor(l))
-           return false;
         Piece p = l.getPiece();
+        if (!super.notSameColor(p))
+           return false;
+        
            
     }
 }
