c2f6814725fc58e4190bb94e23c7982bd6734253
KDerby123 <kika@taom.com>
2016-04-13 14:24:24 -0600

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,7 +1,15 @@
 
 public class Bishop extends Piece {
 	
-	public Bishop(String c) {
-		super(c);
-	}
+     public Bishop(String c,int x,int y) {
+          super(c,x,y);
+     }
+	
+     public boolean testMove(Board b, int x, int y) { //Moves diagonally and vertically
+        Location l = b.getLocAt(x,y);
+        if (!super.notSameColor(l))
+           return false;
+        Piece p = l.getPiece();
+           
+    }
 }
