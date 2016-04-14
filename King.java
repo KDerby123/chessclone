cf7b3cd5a20e04f278d3f7440f722dd9173961bc
KDerby123 <kika@taom.com>
2016-04-14 13:56:00 -0600

    Implemented isInCheck

--- King.java
+++ King.java
@@ -1,28 +1,30 @@
 
 public class King extends Piece {
 	
-    public King(String color, int num, int letter) {
-    	super(color,num,letter);
-    }
+    	public King(String color, int num, int letter) {
+    		super(color,num,letter);
+	}
     
-    public boolean impededCheck(Board board, int num, int letter) { //skeleton
-    	return false //Kings can't be impeded
-    }
+	public boolean impededCheck(Board board, int num, int letter) { //skeleton
+    		return false; //Kings can't be impeded
+	}
 	
-    public boolean testMove(Board board, int num, int letter) { //Tests whether the move is in a 1 square radius and a valid move.
-        Location l = b.getLocAt(num,letter);
-        Piece p = l.getPiece();
-        if (!super.notSameColor(p))
-           return false;
-        return ((Math.abs(super.getNum()-num) <= 1) && (Math.abs(super.getLetter()-letter) <= 1));
-    }
+	public boolean testMove(Board board, int num, int letter) { //Tests whether the move is in a 1 square radius and a valid move.
+        	Location l = b.getLocAt(num,letter); 
+        	Piece p = l.getPiece();
+        	if (!super.notSameColor(p))
+        		return false;
+        	return ((Math.abs(super.getNum()-num) <= 1) && (Math.abs(super.getLetter()-letter) <= 1));
+    	
+	}
 	
-	public boolean isInCheck() { // This method will check if the king is in check
-		//code 
+	public boolean isInCheck(Board board, Player p) { // This method will check if the king is in check
+		ArrayList<Piece> pieces = p.getPieces();
+		for (Piece p : pieces) {
+			if (p.testMove(board,super.getNum(),super.getLetter())
+				return true;
 		return false;
 	}
 	
-	public boolean checkMate() {
-		
-	}
+	
 }
