e07c589cb19f8d8de5f3f7222d73ff57d6571b42
KDerby123 <kika@taom.com>
2016-04-14 12:45:08 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,9 +1,20 @@
 import java.util.ArrayList;
 public abstract class Player {
 	private ArrayList<Piece> pieces;
+	private ArrayList<String> moves;
 	public abstract void makeMove();
 	
 	public ArrayList<Piece> getPieces() {
 		return pieces;
+	}
+	
+	public void addMove(String move) {
+		moves.add(move);
+	}
+	
+	public String getLastMove() {
+		if (moves.size() > 0)
+			return moves.get(moves.size()-1);
+		return "No Moves";
 	}
 }
