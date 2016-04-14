16ac4c37f9b6589e9f56efb0855c208f123aa867
KDerby123 <kika@taom.com>
2016-04-14 12:27:12 -0600

    Update King.java

--- King.java
+++ King.java
@@ -1,16 +1,20 @@
 
 public class King extends Piece {
 	
-    public King(String c) {
-		super(c);
+    public King(String color, int num, int letter) {
+    	super(color,num,letter);
+    }
+    
+    public boolean impededCheck(Board board, int num, int letter) { //skeleton
+    	return false //Kings can't be impeded
     }
 	
-    public boolean testMove(Board b, int x, int y) { //I will do this one - Kika
-        Location l = b.getLocAt(x,y);
-        if (!super.notSameColor(l))
-           return false;
+    public boolean testMove(Board board, int num, int letter) { //I will do this one - Kika
+        Location l = b.getLocAt(num,letter);
         Piece p = l.getPiece();
-           
+        if (!super.notSameColor(p))
+           return false;
+        return ((Math.abs(super.getNum()-num) <= 1) && (Math.abs(super.getLetter()-letter) <= 1));
     }
 	
 	public boolean isInCheck() { // This method will check if the king is in check
