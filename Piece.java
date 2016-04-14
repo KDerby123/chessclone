b7ae461a5a9fac9dbe83c3d7abe756f696589de0
KDerby123 <kika@taom.com>
2016-04-14 12:10:49 -0600

    Update Piece.java
    Added abstract method impededCheck()

--- Piece.java
+++ Piece.java
@@ -1,21 +1,23 @@
 public abstract class Piece{
         private String color;
         private String notatLoc;
-	private int xLoc;
-        private int yLoc;
+	private int numLoc;
+        private int letterLoc;
 	
 	
-	public Piece(String c, int x, int y) {
-		xLoc = x;
-                yLoc = y;
+	public Piece(String c, int num, int letter) {
+		numLoc = x;
+                letterLoc = y;
                 color = c;
                 notatLoc = Location.procCoords(x,y);
 	}
 	
-        public abstract boolean testMove(Board b, int x, int y); //tests whether the move imported is a valid move
+	public abstract boolean impededCheck(Board b, int num, int letter); //this checks if the move provided is impeded; used with
+	//testMove() method
+        public abstract boolean testMove(Board b, int num, int letter); //tests whether the move imported is a valid move
 
         public boolean notSameColor(Location l) { //Tests whether the piece at the Location is of the same color, returns true if not
-                return ((l.getPiece() == null) || (!l.getPiece().getColor().equals(getColor())));
+                return ((l.getPiece() == null) || (!l.getPiece().getColor().equals(color)));
         }
 
         public String getColor() {
