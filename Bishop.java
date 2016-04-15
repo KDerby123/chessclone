1d80b2318cc4b853c8978487749815df727cccab
Dave <imola.david72@gmail.com>
2016-04-15 10:36:58 -0600

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -5,13 +5,17 @@ public class Bishop extends Piece {
           super(color,num,letter);
      }
 	
+	@Override
      public boolean impededCheck(Board board, int num, int letter) { //skeleton
+    	
+    	
     	
      }
      
+     @Override
      public boolean testMove(Board board, int num, int letter) { //Moves diagonally and vertically
-        Location l = b.getLocAt(num,letter);
-        Piece p = l.getPiece();
+        Location loc = board.getLocAt(num,letter);
+        Piece p = loc.getPiece();
         if (!super.notSameColor(p))
            return false;
        
