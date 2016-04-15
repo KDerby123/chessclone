b77345ea30ff13dade5d225834ead49e8a29b32d
KDerby123 <kika@taom.com>
2016-04-15 14:35:14 -0600

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -9,8 +9,8 @@ public class Bishop extends Piece {
      	public boolean impededCheck(Board board, int num, int letter) { //skeleton
      		int selNum = super.getNum();
      		int selLetter = super.getLetter();
-     		int numInc = genInc(selNum,num);
-     		int letterInc = genInc(selLetter,letter);
+     		int numInc = Piece.genInc(selNum,num);
+     		int letterInc = Piece.genInc(selLetter,letter);
      		while ((selNum != num) && (selLetter != letter) {
      			if (!board.isEmpty(selNum,selLetter))
      				return true;
