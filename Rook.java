48b4147a147e3c61db2ac5c1983c4d6182d78e64
KDerby123 <kika@taom.com>
2016-04-15 14:36:43 -0600

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -6,7 +6,17 @@ public class Rook extends Piece {
     	}
     	
     	public boolean impededCheck(Board board, int num, int letter) { //skeleton
-    		
+    		int selNum = super.getNum();
+		int selLetter = super.getLetter();
+		int numInc = Piece.genInc(selNum,num);
+		int letterInc = Piece.genInc(selLetter,letter);
+    		while ((selNum != num) || (selLetter != letter)) {
+    			if (!board.isEmpty(selNum,selLetter)
+    				return true;
+    			selNum += numInc;
+    			selLetter += letterInc;
+    		}
+    		return false;
     	}
 	
     	public boolean testMove(Board board, int num, int letter) { //Moves horizontally and vertically
