7d6be90930882f7472fcdaf38e42d5218a0baa6e
KDerby123 <kika@taom.com>
2016-04-15 14:32:17 -0600

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,19 +1,21 @@
 
 public class Rook extends Piece {
 
-    public Rook(String color,int num,int letter) {
-	super(color,num,letter);
-    }
-    
-    public boolean impededCheck(Board board, int num, int letter) { //skeleton
+    	public Rook(String color,int num,int letter) {
+		super(color,num,letter);
+    	}
     	
-    }
+    	public boolean impededCheck(Board board, int num, int letter) { //skeleton
+    		
+    	}
 	
-    public boolean testMove(Board board, int num, int letter) { //Moves horizontally and vertically
-        Location l = b.getLocAt(num,letter);
-        Piece p = l.getPiece();
-        if (!super.notSameColor(p))
-           return false;
-           
-    }
+    	public boolean testMove(Board board, int num, int letter) { //Moves horizontally and vertically
+        	Location l = b.getLocAt(num,letter);
+        	Piece p = l.getPiece();
+        	if (!super.notSameColor(p))
+           		return false;
+        	if ((super.getNum() != num) && (super.getLetter() != letter))
+        		return false;
+        	return impededCheck(board,num,letter);
+    } 
 }
