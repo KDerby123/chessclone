69a657c7e12b94b9be5f6a6a5e4d868aba605c47
xaephyrus <djschlatt@gmail.com>
2016-04-21 15:55:41 -0600

    Update Chess.java

--- Chess.java
+++ Chess.java
@@ -1,4 +1,27 @@
 //This will be the driver class.
-public class Chess {
+import javax.swing.*; 
 
+
+public class Chess {
+  private static void createAndShowGUI() {
+       
+        JFrame frame = new JFrame("CHESS CLONE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Welcome to Chess");
+        frame.getContentPane().add(label);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
+  
 }
