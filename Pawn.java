edfb52956ba894d16daafa97f10f186aca3bad15
KDerby123 <kika@taom.com>
2016-04-21 15:47:13 -0600

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -8,25 +8,27 @@ public class Pawn extends Piece {
 	}
 	
 	@Override
-	public boolean impededCheck(Board board,int num, int letter) { //implemented abstract method from Piece; returns true if Pawn is
-		return (!board.isEmpty(num-1,letter)) //impeded on it's path to the square
+	public boolean impededCheck(Board board, Coordinate coord) { //implemented abstract method from Piece; returns true if Pawn is
+		return (!board.isEmpty(coord)) //impeded on it's path to the square
 	}
 	
 	@Override
-	public boolean testMove(Board board, int num, int letter) { //I'll do this, as it has a weird move scheme
-	        Location l = board.getLocAt(num,letter) ;
+	public boolean testMove(Board board, Coordinate coord) { //I'll do this, as it has a weird move scheme
+	        Location l = board.getLocAt(coord) ;
 	        Piece p = l.getPiece();
-		if (!super.notSameColor(p) || !(p instanceof King)) //checks whether the piece is of the same color, A king check is not
+	        int num = coord.getNum();
+	        int letter = coord.getLetter();
+		if (!super.notSameColor(p)) //checks whether the piece is of the same color, A king check is not
 		   	return false; //needed due to the fact that this would be an instance of checkmate
 		else
 			if (p == null)  //if location is empty
 				if ((super.getLetter() == letter) //If piece is on same letter rank
-					if ((super.getNum() == num-2) && (!impededCheck(board,num,letter))) //2 square forward move check
+					if ((super.getNum() == num-2) && (!impededCheck(board,coord)) //2 square forward move check
 						return hasMoved; //returns whether this Pawn has moved
 					else
 						return super.getNum() == num-1; //returns the only other possible move: 1 square forward
 				else if ((Math.abs(super.getLetter()-letter) == 1) && (super.getNum() == num-1)) //en passant check
-					return (board.getPlayerW().getLastMove().equals(Location.procCoords(num-1,letter)));
+					return (board.getPlayerW().getLastMove().equals(Coordinate.notatedPos(num-1,letter)));
 				else
 					return false; //returns false atfer checking all possible cases
 			else
